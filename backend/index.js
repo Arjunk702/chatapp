0f1292f24fc24a23d5290a02f4d2feee2b5f60ac
Arjun K <arjunk@example.com>
2026-04-20 12:11:42 +0530

    changed the cors

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,17 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+app.use(cors({
+  origin: 'https://chatapp-a1vu.vercel.app',
+  credentials: true
+}));
 
 // Routes
 app.use("/api/auth", authRoutes);
